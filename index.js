22d9f1b1f94faa1b9a62ce479508b1d3172b3c1b
Abdi Setiawan <bedikadiryt@gmail.com>
2024-06-25 02:39:53 +0700

    update

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
 const ErrorHandler = require('./ErrorHandler')
+const session = require('express-session');
+const flash = require('connect-flash');
 
 /* Models */
 const Product = require('./models/product')
+const Garment = require('./models/garment')
 
 // connect to mongodb
 mongoose.connect('mongodb://127.0.0.1/shop_db')
@@ -20,6 +23,18 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.flash_message = req.flash('flash_message')
+    next()
+})
+
 
 function wrapAsync(fn) {
     return function (req, res, next) {
@@ -27,9 +42,53 @@ function wrapAsync(fn) {
     }
 }
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+// /garments/:garment_id/product/create
+app.get('/garments/:garment_id/product/create', (req, res) => {
+    const {garment_id} = req.params
+    res.render('products/create', {garment_id})
 })
+// /garments/:garment_id/product/
+app.post('/garments/:garment_id/product', wrapAsync(async (req, res) => {
+    const {garment_id} = req.params
+    const product = new Product(req.body)
+    const garment = await Garment.findById(garment_id)
+    garment.products.push(product)
+    await garment.save()
+    product.garment = garment
+    product.save()
+    res.redirect(`/garments/${garment_id}`)
+}))
+// app.get('/garments/:garment_id/product/')
+// /garments/:garment_id/product/:product_id/edit
+// /garments/:garment_id/product/:product_id
+
+app.get('/', (req, res) => {
+    res.send('Halaman sederhana')
+})
+
+app.get('/garments', wrapAsync(async (req, res) => {
+    const garments = await Garment.find({})
+    res.render('garments/index', {garments})
+}))
+app.get('/garments/create', (req, res) => {
+    res.render('garments/create')
+})
+app.post('/garments', wrapAsync(async (req, res) => {
+    const garments = new Garment(req.body)
+    await garments.save()
+    req.flash('flash_message', 'data berhasil ditambahkan! ya bro')
+    res.redirect('/garments')
+}))
+app.get('/garments/:id', wrapAsync(async (req,res) => {
+    const {id} = req.params
+    const garments = await Garment.findById(id).populate('products')
+    res.render('garments/show', {garments})
+}))
+app.delete('/garments/:id', wrapAsync(async (req, res) => {
+    const {id} = req.params
+    await Garment.findOneAndDelete({_id: id})
+    res.redirect('/garments')
+}))
 
 app.get('/products', async (req, res) => {
     const { category } = req.query
@@ -54,7 +113,7 @@ app.post('/products', wrapAsync(async (req, res) => {
 
 app.get('/products/:id', wrapAsync(async (req, res) => {
     const { id } = req.params
-    const product = await Product.findById(id)
+    const product = await Product.findById(id).populate('garment')
     res.render('products/show', { product })
 }))
 
